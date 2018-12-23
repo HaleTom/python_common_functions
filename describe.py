d70e9cba60b3e43d52b7082f228319070f716c25
Tom "Ravi" Hale <tom@hale.ee>
2018-12-23 12:07:49 +1100

    describe: print array shape or string length and str() representation

--- describe.py
+++ describe.py
@@ -1,6 +1,9 @@
-# Print the function call, the class, str representation, line and filename
+# Print the line and filename, function call, the class, str representation and some other info
 
-# From https://stackoverflow.com/a/8856387/5353461
+# Inspired by https://stackoverflow.com/a/8856387/5353461
+import inspect
+import re
+
 def describe(arg):
     frame = inspect.currentframe()
     callerframeinfo = inspect.getframeinfo(frame.f_back)
@@ -10,9 +13,21 @@ def describe(arg):
         m = re.search(r'describe\s*\((.+?)\)$', caller_lines)
         if m:
             caller_lines = m.group(1)
-            print(caller_lines)
             position = str(callerframeinfo.filename) + "@" + str(callerframeinfo.lineno)
-            print("describe(" + caller_lines + ") = " + arg.__class__.__name__ + "(" + str(arg) + ")", position)
+
+            # Add additional info such as array shape or string length
+            additional = ''
+            if hasattr(arg, "shape"):
+                additional += "[shape={}]".format(arg.shape)
+            elif hasattr(arg, "__len__"):  # shape includes length information
+                additional += "[len={}]".format(len(arg))
+
+            # Use str() representation if it is printable
+            str_arg = str(arg)
+            str_arg = str_arg if str_arg.isprintable() else repr(arg)
+
+            print(position, "describe(" + caller_lines + ") = ", end='')
+            print(arg.__class__.__name__ + "(" + str_arg + ")", additional)
         else:
             print("Describe: couldn't find caller context")
 
@@ -20,3 +35,11 @@ def describe(arg):
         del frame
         del callerframeinfo
 
+#
+# import numpy
+#
+# describe((3, 2))
+# describe(numpy.zeros((2, 4)))
+# describe("fo\\o\n")
+# describe('foo' + 'bar')
+# describe(5/2)
